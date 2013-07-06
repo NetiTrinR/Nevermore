2251496ffe16c26d615ef8518c03eb79b28b8c66
NetiTinR <netitrinr@gmail.com>
2013-07-06 00:13:07 -0600

    Fixed routing bug

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,11 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+//Get Methods
+//die(var_dump($_SERVER));
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::controller('/forum', 'ForumController', ['getIndex'=>'forum']);
+Route::controller('/user', 'UserController', ['getRegister'=>'register', 'getLogin' => 'login']);
+Route::controller('/shop', 'ShopController', ['getIndex'=>'shop', 'getDonate'=>'donate']);
+Route::controller('/calendar', 'CalendarController', ['getIndex'=>'calendar']);
+Route::controller('/', 'HomeController', ['getHome'=>'home', 'getHello'=>'hello']);
